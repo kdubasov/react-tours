072e22b9925b3bcb9752d264a4c2da9d933b6aaf
Kirill Dubasov <cergocergo41@gmail.com>
2024-07-13 11:36:04 +0300

    1) add useEscapeListener.ts

--- src/ShowTipsButton.tsx
+++
@@ -1,1 +0,0 @@
-import { useTips } from '@/app/main.ts';const ShowTipsButton = () => {  const { setIsShow } = useTips();  return <button onClick={() => setIsShow(true)}>Показать тур</button>;};export default ShowTipsButton;
