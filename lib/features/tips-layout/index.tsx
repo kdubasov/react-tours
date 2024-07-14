ba7e63450edf41bb0684516b0b41d67c947e8f45
Kirill Dubasov <cergocergo41@gmail.com>
2024-07-14 15:38:58 +0300

    1) add throw error if we havnt blocks in DOM

--- lib/features/tips-layout/index.tsx
+++ lib/features/tips-layout/index.tsx
@@ -1,18 +1,35 @@
 import { useTips } from '@/app/main.ts';
 import TipsActiveLayout from '@/features/tips-active-layout';
+import { TipDataItemWithNode } from '@/shared/types';
 
 type Props = {
   children: React.ReactNode;
+};
+type ActiveProps = {
+  data: TipDataItemWithNode[] | null;
+  isShow: boolean;
 };
 
 const TipsLayout = ({ children }: Props) => {
   const { data, isShow } = useTips();
   return (
     <>
-      {isShow && data && <TipsActiveLayout data={data?.filter((elem) => elem?.node).sort((a, b) => a.idx - b.idx)} />}
+      <TipsActiveLayoutRender data={data} isShow={isShow} />
       {children}
     </>
   );
 };
 
 export default TipsLayout;
+
+const TipsActiveLayoutRender = ({ isShow, data }: ActiveProps) => {
+  if (!isShow || !data) return;
+  const dataWithNodes = data.filter((elem) => elem?.node);
+  if (dataWithNodes.length === 0) {
+    throw new Error(
+      'You are trying to show a tour, but the DOM nodes for this tour are not found in the DOM Tree, please check it!',
+    );
+  }
+
+  return <TipsActiveLayout data={dataWithNodes.sort((a, b) => a.idx - b.idx)} />;
+};
